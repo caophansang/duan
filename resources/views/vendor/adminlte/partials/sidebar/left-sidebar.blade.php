f26f85ba74f7288e0723f7cc54ed5a5e08e49c43
trannhon70 <kevintran351996@gmail.com>
2024-07-16 13:44:55 +0700

    update

--- resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -2,28 +2,24 @@
 
     {{-- Sidebar brand logo --}}
     @if(config('adminlte.logo_img_xl'))
-    @include('adminlte::partials.common.brand-logo-xl')
+        @include('adminlte::partials.common.brand-logo-xl')
     @else
-    @include('adminlte::partials.common.brand-logo-xs')
+        @include('adminlte::partials.common.brand-logo-xs')
     @endif
 
     {{-- Sidebar menu --}}
     <div class="sidebar">
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column {{ config('adminlte.classes_sidebar_nav', '') }}" data-widget="treeview" role="menu" @if(config('adminlte.sidebar_nav_animation_speed') !=300) data-animation-speed="{{ config('adminlte.sidebar_nav_animation_speed') }}" @endif @if(!config('adminlte.sidebar_nav_accordion')) data-accordion="false" @endif>
-                {{-- Configured sidebar links --}}
-                <!-- @each('adminlte::partials.sidebar.menu-item', $adminlte->menu('sidebar'), 'item') -->
-
-                @foreach($adminlte->menu() as $item)
-                @if (Auth::user()->role === 3 || (
-                    $item['url'] == 'admin/products'
-                    || $item['url'] == 'admin/services'
-                    || $item['url'] == 'admin/quantities'
-                )) 
-                @include('adminlte::partials.sidebar.menu-item', $item)
+        <nav class="pt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column {{ config('adminlte.classes_sidebar_nav', '') }}"
+                data-widget="treeview" role="menu"
+                @if(config('adminlte.sidebar_nav_animation_speed') != 300)
+                    data-animation-speed="{{ config('adminlte.sidebar_nav_animation_speed') }}"
                 @endif
-                @endforeach
-
+                @if(!config('adminlte.sidebar_nav_accordion'))
+                    data-accordion="false"
+                @endif>
+                {{-- Configured sidebar links --}}
+                @each('adminlte::partials.sidebar.menu-item', $adminlte->menu('sidebar'), 'item')
             </ul>
         </nav>
     </div>
