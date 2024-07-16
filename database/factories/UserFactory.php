d052c120031df850866ce2dd12968de5786ceb93
nhontx <kevintran351996@gmail.com>
2024-07-16 20:46:29 +0700

    update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'provider' => 'facebook',
             'provider_id' => 3453453463267,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$h7vmF1tgXu0OAg.BvmZrjOs38vLoRJXAXAtZ4jI7X4ZsnkSM/HT0m', // password
             'remember_token' => Str::random(10),
             'role' => 3,
         ];
